846aa7c93af6ece276ead6a043b87ac5bf9fd95f
surysanchez <surysanchezsales@gmail.com>
2023-05-16 09:37:58 -0400

    MERN-Stack Infrastructure

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,19 @@
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import { useState } from "react";
+
+
+export default function AuthPage({ setUser }) {
+  const [showForm, setShowForm] = useState(true)
+  return (
+   
+    <main className="auth-page">
+       <div>SEI</div>
+    <div>CAFE</div>
+        <button className="not-btn" onClick={() => setShowForm(!showForm)} >
+        {showForm ? "SIGN UP" : "SIGN IN"}
+        </button>
+      { showForm ? <SignUpForm  setUser={setUser} /> :  <LoginForm  setUser={setUser} /> }
+    </main>
+  );
+}
